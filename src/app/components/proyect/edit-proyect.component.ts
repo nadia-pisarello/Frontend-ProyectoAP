c6cbb1b7af125929f49d7a8e5b874a0ce57eabab
Nadia Pisarello <pisarello.nads@gmail.com>
2022-10-31 05:55:20 -0300

    update readme

--- src/app/components/proyect/edit-proyect.component.ts
+++ src/app/components/proyect/edit-proyect.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Proyect } from 'src/app/model/proyect';
+import { PortfolioService } from 'src/app/service/portfolio.service';
+import { UploadService } from 'src/app/service/upload.service';
 
 @Component({
   selector: 'app-edit-proyect',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-proyect.component.css']
 })
 export class EditProyectComponent implements OnInit {
-
-  constructor() { }
+  proyect: Proyect = null;
+  constructor( private proyectService: PortfolioService, private router:Router,
+    public uploadService: UploadService,
+    private activated: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.activated.snapshot.params['id'];
+    this.proyectService.getOneProyect(id).subscribe(
+      data => {
+        this.proyect = data      
+      }
+    )
   }
 
+  onUpdate() {
+    const id = this.activated.snapshot.params['id'];
+    this.proyectService.putProyecto(id, this.proyect).subscribe(
+      data => {
+        this.router.navigate(['']);
+      }
+    )
+  }
+
+  selectFile($event: any){
+    const id = this.activated.snapshot.params['id'];
+    const name = 'proyect_'+ id ;
+    this.uploadService.uploadFile($event,name);
+  }
 }
